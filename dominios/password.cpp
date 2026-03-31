3493dea1ed38fe5497f8f3536b28fa5ca65e9ebc
felipeab-b <felipeabferreira22@gmail.com>
2026-03-31 10:43:41 -0300

    fix: validate do password domain

--- dominios/password.cpp
+++ dominios/password.cpp
@@ -1,20 +1,29 @@
 #include "password.hpp"
 
-void Password::validate(std::string password) {
-    if (password.empty() || password.length() > 6) {
+void Password::validate(string password) {
+    if (password.length() != 6)
         throw invalid_argument("Invalid size");
-    }
-    for (int i; i < password.length(); i++) {
-        if (!isalnum(password[i])) throw invalid_argument("Invalid format");
-        if (isdigit(password[i]) & isdigit(password[i + 1])) throw invalid_argument("Invalid format");
-        if (isalpha(password[i]) & isalpha(password[i + 1])) throw invalid_argument("Invalid format");
 
-        int min, mai, dig = 0;
+    int min = 0, mai = 0, dig = 0;
+
+    for (int i = 0; i < (int)password.length(); i++) {
+        if (!isalnum(password[i]))
+            throw invalid_argument("Invalid format");
+
+        if (i < 5) {
+            if (isdigit(password[i]) && isdigit(password[i + 1]))
+                throw invalid_argument("Invalid format");
+            if (isalpha(password[i]) && isalpha(password[i + 1]))
+                throw invalid_argument("Invalid format");
+        }
+
         if (isdigit(password[i])) dig++;
         if (islower(password[i])) min++;
         if (isupper(password[i])) mai++;
-        if (dig == 0 || min == 0 || mai == 0) throw invalid_argument("Invalid format");
     }
+
+    if (dig == 0 || min == 0 || mai == 0)
+        throw invalid_argument("Invalid format");
 }
 
 void Password::set(string password) {
