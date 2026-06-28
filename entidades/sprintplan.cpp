e87113a35691bdec3079dd320e946be0ee6899c6
markvncs <vmarcu090@gmail.com>
2026-06-28 16:57:47 -0300

    feat: sprintplan.hpp e sprintplan.cpp devidamente separados

--- entidades/sprintplan.cpp
+++ entidades/sprintplan.cpp
@@ -1,1 +1,25 @@
+#include "sprintplan.hpp"
 
+SprintPlan::SprintPlan(Code c) { 
+    code = c; 
+}
+
+void SprintPlan::setObjective(Text o) { 
+    objective = o; 
+}
+
+void SprintPlan::setCapacity(Time c) { 
+    capacity = c; 
+}
+
+Code SprintPlan::getCode() const { 
+    return code; 
+}
+
+Text SprintPlan::getObjective() const { 
+    return objective; 
+}
+
+Time SprintPlan::getCapacity() const { 
+    return capacity; 
+}
--- entidades/sprintplan.hpp
+++ entidades/sprintplan.hpp
@@ -1,5 +1,6 @@
 #ifndef SPRINTPLAN_HPP
 #define SPRINTPLAN_HPP
+
 #include "../dominios/derivados/code.hpp"
 #include "../dominios/derivados/text.hpp"
 #include "../dominios/derivados/time.hpp"
@@ -20,14 +21,5 @@ public:
   Text getObjective() const;
   Time getCapacity() const;
 };
-
-inline SprintPlan::SprintPlan(Code c) { code = c; }
-
-inline void SprintPlan::setObjective(Text o) { objective = o; }
-inline void SprintPlan::setCapacity(Time c) { capacity = c; }
-
-inline Code SprintPlan::getCode() const { return code; }
-inline Text SprintPlan::getObjective() const { return objective; }
-inline Time SprintPlan::getCapacity() const { return capacity; }
 
 #endif
